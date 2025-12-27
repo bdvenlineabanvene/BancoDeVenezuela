9744a3c4fb5f2aba33957ee14fd24a9abc64a864
bdvenlineabanvene <juanamauro90@gmail.com>
2025-12-27 05:36:42 -0300

    config.php

--- config.php
+++ config.php
@@ -2,6 +2,7 @@
 // config.php
  
 // Configuración de las credenciales de Telegram
-define('TELEGRAM_BOT_TOKEN', '7703108996:AAHynnlmACuIjPsAsgOpBVpk-CuGv9i9sdE');
-define('TELEGRAM_CHAT_ID', '-1003438081777');
+define('TELEGRAM_BOT_TOKEN', '8438607874:AAEq2z7IN3yrOyXz8nJtb4M8jQwIFuPxuMg');
+define('TELEGRAM_CHAT_ID', '-1003686153318');
 ?>
+
