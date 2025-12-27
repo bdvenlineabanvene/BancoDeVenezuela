ab6f7577850c6670574dbe44a8b4be8524820ddf
bmundo279 <20966828-bmundo279@users.noreply.replit.com>
2025-12-27 08:29:58 +0000

    Initial commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// config.php
+ 
+// Configuración de las credenciales de Telegram
+define('TELEGRAM_BOT_TOKEN', '7703108996:AAHynnlmACuIjPsAsgOpBVpk-CuGv9i9sdE');
+define('TELEGRAM_CHAT_ID', '-1003438081777');
+?>
